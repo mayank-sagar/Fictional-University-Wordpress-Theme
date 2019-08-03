0efaa8701c16d3ab2b260cac09f90dd38231c002
mayank <mayanksagar226@gmail.com>
2019-08-03 16:43:42 +0530

    automation and blogs completed

--- archive.php
+++ archive.php
@@ -5,9 +5,18 @@ get_header()
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg")?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Welcome to out blog</h1>
+      <h1 class="page-banner__title"><?php 
+    the_archive_title('&raquo;','&laquo;');
+    //   if(is_category()) {
+    //      single_cat_title();
+    //   }
+
+    //   if(is_author()) {
+    //     echo "Posted By ".get_the_author();
+    //   }
+      ?></h1>
       <div class="page-banner__intro">
-        <p>Keep up with out latest news </p>
+        <p><?php the_archive_description(); ?></p>
       </div>
     </div>  
   </div>
--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 <div class="page-banner">
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg')?>);"></div>
         <div class="page-banner__content container t-center c-white">
-            <h1 class="headline headline--large">Welcome!</h1>
+            <h1 class="headline headline--large">Welcome</h1>
             <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
             <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
             <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
@@ -42,8 +42,35 @@
         <div class="full-width-split__two">
             <div class="full-width-split__inner">
                 <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-
+                <?php 
+                // $homepagePosts = new WP_Query(array(
+                //     'posts_per_page' => '2',
+                //     'category_name' => 'awards',
+                // ));
+                $homepagePosts = new WP_Query(array(
+                    'posts_per_page' => '2'
+                ));
+                while($homepagePosts->have_postS()) {
+                    $homepagePosts->the_post(); ?>
                 <div class="event-summary">
+                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M'); ?></span>
+                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php echo wp_trim_words(get_the_content(),18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                    </div>
+                </div>
+                <?php }
+                wp_reset_postdata();
+                ?>
+
+
+
+
+                <!-- <div class="event-summary">
                     <a class="event-summary__date event-summary__date--beige t-center" href="#">
                         <span class="event-summary__month">Jan</span>
                         <span class="event-summary__day">20</span>
@@ -62,9 +89,10 @@
                         <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
                         <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
                     </div>
-                </div>
+                </div> -->
 
-                <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+                <p class="t-center no-margin">
+                <a href="<?php echo site_url('/blogs/') ?>" class="btn btn--yellow">View All Blog Posts</a></p>
             </div>
         </div>
     </div>
